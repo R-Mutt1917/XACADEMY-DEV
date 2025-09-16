fe968db003dfd7b566d85eb42a04b81dce00f1c3
Matías Zelarayán <matiaszelarayandev@gmail.com>
2025-09-15 23:33:14 -0300

    Agregar cuarto ejercicio de práctica 1 - JS

--- EjercicioPractica1/ejercicio4.js
+++ EjercicioPractica1/ejercicio4.js
@@ -7,3 +7,17 @@ Realizar un script que escriba una pirámide del 1 al 6 de la siguiente forma:
   12345
   123456
 */
+function imprimirPiramide() {
+  // Iterar del 1 al 6
+  for (let i = 1; i <= 6; i++) {
+    let linea = '';
+    // Construir la línea con números secuenciales
+    for (let j = 1; j <= i; j++) {
+      linea += j;
+    }
+    // Imprimir la línea con un salto de línea adicional
+    console.log(linea + '\n');
+  }     
+}
+// Ejecutar la función
+imprimirPiramide(); 
