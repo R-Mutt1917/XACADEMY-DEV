ef9a76c0d7fff26a31937bae69fae0ef86cfc633
Matías Zelarayán <matiaszelarayandev@gmail.com>
2025-09-15 23:15:57 -0300

    Agregar tercer ejercicio resuelto: Ejercicio de práctica 1 - JS

--- EjercicioPractica1/ejercicio3.js
+++ EjercicioPractica1/ejercicio3.js
@@ -6,3 +6,14 @@
   55555
   666666
   */
+ function imprimirPiramide() {
+    // Iterar del 1 al 6
+    for (let i = 1; i <= 6; i++) {
+        // Crear una cadena con el número repetido i veces
+        let linea = String(i).repeat(i);
+        // Imprimir la línea con un salto de línea adicional
+        console.log(linea + '\n');
+    }
+}
+// Ejecutar la función 
+imprimirPiramide();
