a72098e26a0275079c7da23858cfeba0cf7fd4cc
Matías Zelarayán <matiaszelarayandev@gmail.com>
2025-09-27 16:28:02 -0300

    practica atributos y directivas ngIf + ngFor

--- angular/src/app/app.ts
+++ angular/src/app/app.ts
@@ -1,14 +1,34 @@
 import { Component, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
+  imports: [CommonModule]
 })
 export class App {
-  contador = 0;
+  tablaPuntuaciones = [
+    {nombre: 'Juan', puntuacion: 855},
+    {nombre: 'Pedro', puntuacion: 803},
+    {nombre: 'Luis', puntuacion: 720},
+  ];
 
-  incrementar() {
-    this.contador++;
+  contador = 0;
+  bonus = false;
+
+  sumar(cantidad: number): void {
+    this.contador = this.contador + cantidad;
+
+    // cuando alcanzamos 10, habilita un botón de bonus
+    if (this.contador === 10) {
+      this.bonus = true;
+
+      // timeout de 5 segundos, pasado este tiempo el botón volverá a su estado original
+      window.setTimeout(() => {
+        this.bonus = false;
+      }, 5000);
+    }
   }
 }
+
