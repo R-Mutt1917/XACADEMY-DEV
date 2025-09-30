85bff7d192dcc145d8140adb799fe60468fd160e
Matías Zelarayán <matiaszelarayandev@gmail.com>
2025-09-30 11:37:05 -0300

    cambios logica component

--- angular/src/app/app.ts
+++ angular/src/app/app.ts
@@ -1,11 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Clicker } from './clicker/clicker';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   templateUrl: './app.html',
   styleUrl: './app.scss',
-  imports: [CommonModule]
+  imports: [CommonModule, Clicker]
 })
 export class App {
   tablaPuntuaciones = [
@@ -13,22 +15,5 @@ export class App {
     {nombre: 'Pedro', puntuacion: 803},
     {nombre: 'Luis', puntuacion: 720},
   ];
-
-  contador = 0;
-  bonus = false;
-
-  sumar(cantidad: number): void {
-    this.contador = this.contador + cantidad;
-
-    // cuando alcanzamos 10, habilita un botón de bonus
-    if (this.contador === 10) {
-      this.bonus = true;
-
-      // timeout de 5 segundos, pasado este tiempo el botón volverá a su estado original
-      window.setTimeout(() => {
-        this.bonus = false;
-      }, 5000);
-    }
-  }
 }
 
--- angular/src/app/clicker/clicker.ts
+++ angular/src/app/clicker/clicker.ts
@@ -1,11 +1,29 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-clicker',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './clicker.html',
   styleUrl: './clicker.scss'
 })
 export class Clicker {
+  contador = 0;
+  bonus = false;
+
+  sumar(cantidad: number): void {
+    this.contador = this.contador + cantidad;
+
+    // cuando alcanzamos 10, habilita un botón de bonus
+    if (this.contador === 10) {
+      this.bonus = true;
+
+      // timeout de 5 segundos, pasado este tiempo el botón volverá a su estado original
+      window.setTimeout(() => {
+        this.bonus = false;
+      }, 5000);
+    }
+  }
 
 }
